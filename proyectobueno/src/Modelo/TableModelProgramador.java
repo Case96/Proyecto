a4a93456bcc0178bf5c12c142d512b1e5097ec7a
Case96 <103947792+Case96@users.noreply.github.com>
2022-06-06 13:55:30 +0200

    Add files via upload

--- proyectobueno/src/Modelo/TableModelProgramador.java
+++ proyectobueno/src/Modelo/TableModelProgramador.java
@@ -7,8 +7,76 @@ package Modelo;
 
 /**
  *
- * @author Alex
+ * @author josem
  */
-public class TableModelProgramador {
+import Controlador.Controlador;
+import Controlador.ControladorProgramador;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import javax.swing.table.AbstractTableModel;
+/**
+ *
+ * @author MEDAC
+ */
+public  class TableModelProgramador extends AbstractTableModel {
+    private static final String[] columnNames = { "","","",""};
     
+    private final LinkedList<Proyecto> list;
+
+    
+    public TableModelProgramador() {
+    list = new LinkedList<>();
+        
+    }
+    public Proyecto getValueAt(int rowIndex) {
+        return list.get(rowIndex);
+    }
+
+    
+    public void cargarProgramadores(String programador) throws SQLException, Exception {
+        // Obtiene la lista de programadores de la BD
+        ArrayList<Proyecto> proyectos = ControladorProgramador.getInstance().obtenerProgramador(programador);
+        System.out.println(proyectos.size());
+        // Borra el contenido anterior y añade el nuevo.
+        list.clear();
+        list.addAll(proyectos);
+        // Notifica a la vista que el contenido ha cambiado para que se refresque.
+        fireTableDataChanged();
+    }
+    
+      @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+    @Override
+    public int getRowCount() {
+        return list.size();
+    }
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return list.get(rowIndex).getDni();
+            case 1:
+                return list.get(rowIndex).getNombre();
+            
+            case 2:
+                return list.get(rowIndex).getTitulo();
+            case 3:
+                return list.get(rowIndex).getIdiomas();
+            case 4:
+                return list.get(rowIndex).getEstudios();
+            case 5:
+                return list.get(rowIndex).getCargo();
+                
+            case 6:
+                return list.get(rowIndex).getProgramador();
 }
+
+    
--- proyectobueno/src/Modelo/TableModelProyecto.java
+++ proyectobueno/src/Modelo/TableModelProyecto.java
@@ -5,10 +5,72 @@
  */
 package Modelo;
 
+import Controlador.ControladorProyecto;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
- * @author Alex
+ * @author josem
  */
-public class TableModelProyecto {
+
+    public class TableModelProyecto extends AbstractTableModel {
+    private static final String[] columnNames = { "codigo", "tema","duracion"};
+    private final LinkedList<Proyecto> list;
+
     
-}
+    public TableModelProyecto() {
+        list = new LinkedList<>();
+        
+    }
+    public Proyecto getValueAt(int rowIndex) {
+        return list.get(rowIndex);
+    }
+    
+     public  void cargarProyectosProgramador() throws SQLException, Exception {
+        
+        // Obtiene la lista de proyectos de la BD
+        ArrayList<Proyecto> proyectos = ControladorProyecto.getInstance().obtenerProyectos();
+        System.out.println(proyectos.size());
+        
+        
+        // Borra el contenido anterior y añade el nuevo.
+        list.clear();
+        list.addAll(proyectos);
+        // Notifica a la vista que el contenido ha cambiado para que se refresque.
+        fireTableDataChanged();
+    }
+
+    @Override
+ 
+  public int getColumnCount() {
+        return columnNames.length;
+    }
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+    @Override
+    public int getRowCount() {
+        return list.size();
+    }
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            
+                
+            case 0:
+                return list.get(rowIndex).getCodigo();
+            case 1:
+                return list.get(rowIndex).getTema();
+            case 2:
+                return list.get(rowIndex).getDuracion();
+             }
+        return null;
+    }
+
+    }
+
+
